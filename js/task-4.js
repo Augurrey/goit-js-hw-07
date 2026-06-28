1df1277a0c89b460a34f0394bff244469fd3d452
Augurey <hameleon322@gmail.com>
2026-06-28 23:52:17 +0300

    done

---
+++ js/task-4.js
@@ -0,0 +1,21 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+  } else {
+    const formData = {
+      login: email.trim(),
+      password: password.trim(),
+    };
+    console.log(formData);
+    form.reset();
+  }
+}
